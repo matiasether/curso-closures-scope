1af0c1554039eed7839c1ee37823bb81eb25d982
matiasether <matias.ether@protonmail.com>
2023-08-18 18:38:39 -0300

    Curso terminado

--- src/closure/mymoneybox.js
+++ src/closure/mymoneybox.js
@@ -14,3 +14,17 @@ AlanMoneyBox(5, 'Alan')
 const elisaMoneyBox = moneyBox()
 elisaMoneyBox(10, 'Elisa')
 elisaMoneyBox(2, 'Elisa')
+
+function createPetList() {
+    let petList = []
+    return function (addPet = 'Mascota sin nombre') { 
+        petList.push(addPet)  
+        return petList
+    }
+    
+  }
+  const myPetList = createPetList()
+  
+  console.log(myPetList('Michi'))
+  console.log(myPetList('firulais'))
+  console.log(myPetList())
